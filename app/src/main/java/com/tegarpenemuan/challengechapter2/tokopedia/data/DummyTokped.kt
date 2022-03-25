c5bb66ec4d190eb055a230443c0e3119a86b3683
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-03-25 10:09:39 +0700

    Add Icon Tokopedia

--- app/src/main/java/com/tegarpenemuan/challengechapter2/tokopedia/data/DummyTokped.kt
+++ app/src/main/java/com/tegarpenemuan/challengechapter2/tokopedia/data/DummyTokped.kt
@@ -6,28 +6,28 @@ import com.tegarpenemuan.minichallenge2.model.CategoryTokpedModel
 object DummyTokped {
     val CATEGORY_TOKPEDS: List<CategoryTokpedModel> = listOf(
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_officialstore,
+            desc = "Official Store",
         ),
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_lihatsemua,
+            desc = "Lihat Semua",
         ),
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_kebutuhanharian,
+            desc = "Kebutuhan Harian",
         ),
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_hp,
+            desc = "HP & Tablet",
         ),
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_topup,
+            desc = "Top-Up & Tagihan",
         ),
         CategoryTokpedModel(
-            avatar = R.drawable.ic_explore,
-            desc = "GoRide",
+            avatar = R.drawable.ic_keuangan,
+            desc = "Keuangan",
         ),
     )
 }
